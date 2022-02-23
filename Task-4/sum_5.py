4b19078faf496a148a43d9ac796f22715befba3c
Harshi-13 <99636533+Harshi-13@users.noreply.github.com>
2022-02-23 23:50:51 +0530

    Add files via upload

---
+++ Task-4/sum_5.py
@@ -0,0 +1,29 @@
+# Question-5
+# Print the following pattern.
+
+def printstarpyramind(row):
+    # First for creating spaces
+    H = row - 1
+
+    # Second;y create outer loop for the number of rows
+    for i in range(0, row):
+
+        # Then create inner loop to give the spaces
+        for a in range(0, H):
+            print(end=" ")
+
+        # Finally decrement H after every loop
+        H = H - 1
+
+        # inner loop for the number of columns
+        for a in range(0, i + 1):
+            # this will print stars
+            print("* ", end="")
+
+        # ending line after every row
+        print("\r")
+
+
+# Given number of rows = 5
+row = 5
+printstarpyramind(row)
