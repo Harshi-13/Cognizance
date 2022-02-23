713b66db90363c95eecdff6168c4bf03f263d9c6
Harshi-13 <99636533+Harshi-13@users.noreply.github.com>
2022-02-23 23:06:07 +0530

    Add files via upload

---
+++ Task-4/sum_1.py
@@ -0,0 +1,11 @@
+# Question 1
+# Write a program to perform addition on two user input numbers.
+
+# Get the first number from the user.
+first_no = int(input('First Number: '))
+# Get the second number from the user.
+second_no = int(input('Second Number: '))
+# Get the mathematical sum of the numbers
+add = first_no + second_no
+# Print it
+print("Sum: {0}".format(add))
